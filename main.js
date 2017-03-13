3f68efa7aa813b37a0afd4765a95df9ecff50ad4
Daz Jones <daz@dazzozo.com>
2017-03-13 10:31:44 +0000

    Implement getTLS.

--- main.js
+++ main.js
@@ -766,6 +766,10 @@ sploitcore.prototype.svc = function(id, registers, dump_regs) {
 	return this.call(svc_list[id], [], [], registers, dump_regs);
 }
 
+sploitcore.prototype.getTLS = function() {
+	return this.call(0x3ACE54, []);
+}
+
 sploitcore.prototype.queryMem = function(addr, raw) {
 	if(arguments.length == 1)
 		raw = false;
