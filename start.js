c3a5afbff9a9ea9ee71011e5e2dd1d5ab437ccd9
Adam Brady <adam@boxxen.org>
2017-03-19 23:47:43 +1100

    Add startup options

--- start.js
+++ start.js
@@ -10,10 +10,119 @@ const bodyParser = require('body-parser')
 const mkdirp = require('mkdirp')
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const yargs = require('yargs')
 
 if (os.platform() !== 'win32' && process.getuid() !== 0) {
   console.error('Please run as root so we can bind to port 53 & 80')
   process.exit()
+}
+
+let argv = yargs
+  .usage('Usage $0')
+  .describe('disable-curses', 'Disabled curses interface. Requires --logfile')
+  .describe('disable-dns', 'Disables builtin DNS server.')
+  .describe('ip', 'Override IP address DNS server responds with')
+  .describe('logfile', 'Writes debug log to file')
+  .example('$0 --ip 1.2.4.8 --disable-curses --logfile debug.txt')
+  .help('h')
+  .nargs('ip', 1)
+  .nargs('logfile', 1)
+  .alias('h', 'help')
+  .argv
+
+if (argv['disable-curses'] && !argv.logfile) {
+  console.error('--disable-curses requires --logfile')
+  process.exit(1)
+}
+
+let logger = {
+  log: function (data) {
+    fs.writeFileSync(path.resolve(__dirname, argv.logfile), `${data}\n`, {
+      flag: 'a'
+    })
+  }
+}
+
+if (argv['disable-dns'] !== true) {
+  // Spin up our DNS server
+  let dns = dnsd.createServer(function(req, res) {
+    res.end(argv.ip || ip.address())
+  })
+
+  dns.on('error', function (err) {
+    console.log(`There was an issue setting up DNS: ${err.message}`)
+    process.exit()
+  })
+
+  dns.listen(53, '0.0.0.0')
+}
+
+// Web server
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.end(fs.readFileSync(path.resolve(__dirname, 'exploit/index.html')))
+})
+
+app.get('/bundle.js', function (req, res) {
+  let b = browserify({
+    entries: [ 'exploit/main.js'],
+    cache: {},
+    packageCache: {}
+  }).bundle().pipe(res)
+})
+
+let failures = 0
+let successes = 0
+
+app.post('/log', function (req, res) {
+  let message = req.body.msg
+
+  if (message === 'Loaded') {
+    logger.log(`Success percentage: ${(successes / failures * 100).toFixed(2)} (${successes + failures} samples)`)
+  } else if (message === '~~failed') {
+    failures++
+  } else if (message === '~~success') {
+    successes++
+  } else {
+    logger.log(message)
+  }
+
+  return res.sendStatus(200)
+})
+
+app.post('/error', function (req, res) {
+  logger.log(`ERR [${req.body.line}]: ${req.body.message}`)
+  return res.sendStatus(200)
+})
+
+app.post('/filedump', function (req, res) {
+  let name = req.get('Content-Disposition').replace(':', '')
+  let dir = path.dirname(name)
+
+  try {
+    fs.statSync(dir)
+  } catch (e) {
+    mkdirp.sync(dir)
+  }
+  req.pipe(fs.createWriteStream(name, {
+    defaultEncoding: 'binary',
+    flags: 'a'
+  }))
+
+  return res.sendStatus(200)
+})
+
+app.listen(80, '0.0.0.0', function (err) {
+  if (err) {
+    console.error('Could not bind to port 80')
+    process.exit(1)
+  }
+})
+
+if (argv['disable-curses']) {
+  return require('./repl')
 }
 
 // Setup our terminal
@@ -40,6 +149,8 @@ let log = contrib.log({
     }
   }
 })
+
+logger = log
 
 let repl = blessed.terminal({
   parent: screen,
@@ -72,82 +183,6 @@ screen.append(log)
 screen.append(repl)
 
 repl.focus()
-
-// Spin up our DNS server
-let dns = dnsd.createServer(function(req, res) {
-  res.end(process.argv[2] || ip.address())
-})
-
-dns.on('error', function (err) {
-  console.log('There was an issue setting up DNS:', err.message)
-  process.exit()
-})
-
-dns.listen(53, '0.0.0.0')
-
-// Web server
-const app = express()
-app.use(bodyParser.json())
-
-app.get('/', function (req, res) {
-  res.end(fs.readFileSync(path.resolve(__dirname, 'exploit/index.html')))
-})
-
-app.get('/bundle.js', function (req, res) {
-  let b = browserify({
-    entries: [ 'exploit/main.js'],
-    cache: {},
-    packageCache: {}
-  }).bundle().pipe(res)
-})
-
-let failures = 0
-let successes = 0
-
-app.post('/log', function (req, res) {
-  let message = req.body.msg
-
-  if (message === 'Loaded') {
-    log.log(`Success percentage: ${(successes / failures * 100).toFixed(2)} (${successes + failures} samples)`)
-  } else if (message === '~~failed') {
-    failures++
-  } else if (message === '~~success') {
-    successes++
-  } else {
-    log.log(message)
-  }
-
-  return res.sendStatus(200)
-})
-
-app.post('/error', function (req, res) {
-  log.log(`ERR [${req.body.line}]: ${req.body.message}`)
-  return res.sendStatus(200)
-})
-
-app.post('/filedump', function (req, res) {
-  let name = req.get('Content-Disposition').replace(':', '')
-  let dir = path.dirname(name)
-
-  try {
-    fs.statSync(dir)
-  } catch (e) {
-    mkdirp.sync(dir)
-  }
-  req.pipe(fs.createWriteStream(name, {
-    defaultEncoding: 'binary',
-    flags: 'a'
-  }))
-
-  return res.sendStatus(200)
-})
-
-app.listen(80, '0.0.0.0', function (err) {
-  if (err) {
-    console.error('Could not bind to port 80')
-    process.exit(1)
-  }
-})
 
 // Render everything
 screen.render()
