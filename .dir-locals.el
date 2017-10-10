f391ef2e4aa479370b9d62369c7c9a9a68cfba25
misson20000 <xenotoad@xenotoad.net>
2017-10-09 21:25:49 -0700

    add two missing dotfiles

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((tab-width . 2)
+         (c-basic-offset . 2)
+         (indent-tabs-mode . t)
+         (c-auto-align-backslashes . nil)
+		 (c-file-offsets . (
+							(cpp-define-intro . 0)
+							)))))
