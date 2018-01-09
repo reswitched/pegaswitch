0629bd051a045415a6c6ca2029f5be7c1ec267d5
GovanifY <govanify@protonmail.com>
2018-01-09 13:43:56 +0100

    Misc fixes

--- usefulscripts/dumpModules.js
+++ usefulscripts/dumpModules.js
@@ -26,7 +26,7 @@ var modules ={
 		'psc' : '0100000000000021',
 		'capsrv' : '0100000000000022',
 		'am' : '0100000000000023',
-		'ssl' : '0100000000000024', 
+		'ssl' : '0100000000000024',
 		'nim' : '0100000000000025',
 		'lbl' : '0100000000000029',
 		'btm' : '010000000000002A',
@@ -40,18 +40,18 @@ var modules ={
 		'es' : '0100000000000033',
 		'fatal' : '0100000000000034',
 		'creport' : '0100000000000036',
-		
+
 };
 function dumpModule(module, loader, name) {
 	//We need a ILocationResolver to pass to fsp to say what we are reading so we're getting a handle
 	sc.getService("lr", (lripc) => {
 		//3 is the StorageID for NAND System
-		var lr = sc.ipcMsg(0).data(3).sendTo(lripc);
+		var lr = sc.ipcMsg(0).data(3).sendTo(lripc).assertOk();
 		sc.withHandle(lr.movedHandles[0], (content) => {
-		        //We are getting our ContentPath needed for fsp, c being the "receiving" buffer 
+				//We are getting our ContentPath needed for fsp, c being the "receiving" buffer
 			var buf = new ArrayBuffer(0x300);
-			sc.ipcMsg(0).data(utils.parseAddr(module)).cDescriptor(buf).sendTo(content);
-			
+			sc.ipcMsg(0).data(utils.parseAddr(module)).cDescriptor(buf).sendTo(content).assertOk();
+
 			//We are now mounting our code region
 			var fs =sc.ipcMsg(0).datau64(utils.parseAddr(module)).xDescriptor(buf).sendTo(loader).assertOk();
 			sc.withHandle(fs.movedHandles[0], (storage) => {
@@ -66,7 +66,7 @@ function dumpModule(module, loader, name) {
 }
 
 utils.log("stage1, getting webkit ldr:ro handle");
-//We are reusing WebKit's ldr:ro session 
+//We are reusing WebKit's ldr:ro session
 var ldrro_mng_ptr = utils.add2(sc.mainaddr, 0x955558);
 //utils.log('ldr:ro management str base ptr is: ' + utils.paddr(ldrro_mng_ptr));
 var ldrro_mng = sc.read8(ldrro_mng_ptr);
@@ -83,30 +83,34 @@ var nrrbase = sc.malloc(0x1000 + 0xfff);
 var nrrSize = 0x1000;
 var nroSize = 0x1000;
 var bssSize = 0x900;
- 
+
 //We initialize with a Thread Handle, 0xffff8000 instead of current process handle, 0xffff8001
 sc.ipcMsg(4).datau64(0).sendPid().copyHandle(0xffff8000).sendTo(ldrro);
-//We setup a fake nrr loading sequence 
+//We setup a fake nrr loading sequence
 sc.ipcMsg(2).datau64(0, nrrbase, nrrSize).sendPid().sendTo(ldrro);
 
 utils.log("stage3, crashing ldr:ro");
-//Just calling a normal cmd0 will crash since it will call svcMapProcessCodeMemory during LoadNro sequence using a 
+//Just calling a normal cmd0 will crash since it will call svcMapProcessCodeMemory during LoadNro sequence using a
 //thread handle, attempting a process handle. This happens because svcGetProcessInfo in ldr:ro initialize can also take up
 //a Thread Handle as an argument, while svcMapProcessCodeMemory will bug out on it
-sc.ipcMsg(0).datau64(0, nrobase, nroSize, utils.add2(nrobase, nroSize), bssSize).sendPid().sendTo(ldrro);
- 
-utils.log("stage4, connecting to fsp"); 
+var res =sc.ipcMsg(0).datau64(0, nrobase, nroSize, utils.add2(nrobase, nroSize), bssSize).sendPid().sendTo(ldrro);
+
+//Those are useless so better free them now
+sc.free(nrobase);
+sc.free(nrrbase);
+
+utils.log("stage4, connecting to fsp");
 
 sc.getService("fsp-ldr", (hndle) => {
 	//utils.log("Got an handle to fsp: 0x" +hndle.toString(16));
-	utils.log("stage5, dumping sysmodule"); 
+	utils.log("stage5, dumping sysmodule");
 	for (var name in modules) {
 		utils.log("dumping " + name);
 		try {
 			dumpModule(modules[name], hndle, name);
-		    }
-		    catch (e) {
-		    }
+			}
+			catch (e) {
+			}
 	}
 });
 
